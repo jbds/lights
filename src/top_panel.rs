38aff2e76dbf77fad0f3324f712e9a3972824d5c
jonb <sales@jbds.co.uk>
2024-10-12 12:13:47 +0100

    refactor

--- src/top_panel.rs
+++ src/top_panel.rs
@@ -1,2 +1,10 @@
 use crate::utilities;
 use crate::LightsApp;
+
+pub fn get_closure(
+    lights_app: &mut LightsApp,
+) -> impl FnOnce(&mut egui::UI) -> egui::Response + '_ {
+    |ui| {
+        ui.label("top_panel_here");
+    }
+}
