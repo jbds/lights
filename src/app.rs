a606564ce58e18761b655a20965f0d90f1055e21
jonb <sales@jbds.co.uk>
2026-08-20 16:19:57 +0100

    configure for differenet serial port names

--- src/app.rs
+++ src/app.rs
@@ -6,28 +6,27 @@ use crate::panels::top_panel;
 use crate::utilities;
 use crate::utilities::add_after_selected;
 use crate::utilities::{delete_selected, save_selected};
-
-#[cfg(target_arch = "aarch64")]
-use dmx::{self, DmxTransmitter};
 use egui::FontFamily::Proportional;
 use egui::FontId;
 use egui::TextStyle::*;
 use std::time::{Duration, Instant};
+//#[cfg(target_arch = "aarch64")]
+use dmx::{self, DmxTransmitter};
 
-/// We derive Deserialize/Serialize so we can persist app state on shutdown.
-//#[derive(serde::Deserialize, serde::Serialize)]
-//#[serde(default)] // if we add new fields, give them default values when deserializing old state
+
+// app state is only persisted for app lifetime
+// light_records ONLY are read from json file during app initialisation
+// light_records ONLY are written to a json file on click of Delete/Save/AddAfter_Selected
 pub struct LightsApp {
-    //#[serde(skip)] // This how you opt-out of serialization of a field
     pub slider_count: usize,
-    //pub values: Vec<u8>, //stores the current array of light values including master dimmer
-    pub values: Vec<f64>, // the master dimmer needs to use f64 resolution, u8 is to granular
+    //stores the current array of light values including master dimmer, needs to use f64 resolution, u8 is too granular 
+    pub values: Vec<f64>, 
     pub is_master_adjusteds: Vec<bool>,
     pub labels: Vec<String>,
     pub values_adjusted: Vec<f64>,
     pub instant: Instant,   // we need this to check timing
     pub duration: Duration, // ditto
-    #[cfg(target_arch = "aarch64")]
+    //#[cfg(target_arch = "aarch64")]
     pub dmx_port: dmx_serial::posix::TTYPort, //dmx_serial::Result<dmx_serial::posix::TTYPort>, // valid for life of the app
     pub light_records: Vec<(String, Vec<f64>)>, // a list of scene names plus all the slider values before any adjustment by the master slider and master alaways zero
     pub light_records_index: usize,             // initialized to zero
@@ -66,6 +65,10 @@ fn configure_text_styles(ctx: &egui::Context) {
 impl Default for LightsApp {
     fn default() -> Self {
         let slider_count: usize = 25;
+        #[cfg(target_arch = "x86_64")]
+        let serial_port_name = "/dev/ttyUSB0";
+        #[cfg(target_arch = "aarch64")]
+        let serial_port_name = "/dev/serial0"; 
         Self {
             slider_count: slider_count,
             // set all sliders to zero
@@ -106,8 +109,9 @@ impl Default for LightsApp {
             values_adjusted: vec![0.0; slider_count],
             instant: Instant::now(), // func is only called once, so this value will be fixed
             duration: Duration::from_secs(0), // store elapsed time on each screen repaint
-            #[cfg(target_arch = "aarch64")]
-            dmx_port: dmx::open_serial("/dev/serial0").unwrap(), // create the serial port
+            //#[cfg(target_arch = "aarch64")]
+            //dmx_port: dmx::open_serial("/dev/serial0").unwrap(), // create the serial port
+            dmx_port: dmx::open_serial(serial_port_name).unwrap(), // create the serial port
             // light_records: vec![
             //     vec![0; slider_count],
             //     vec![128; slider_count],
@@ -214,7 +218,7 @@ impl eframe::App for LightsApp {
                 &self.instant.elapsed()
             );
             // send a dmx packet, &Vec<u8> can be coerced to &[u8]
-            #[cfg(target_arch = "aarch64")]
+            //#[cfg(target_arch = "aarch64")]
             let _ = self.dmx_port.send_dmx_packet(&self.array_of_u8);
             self.duration = self.instant.elapsed();
         } else {
